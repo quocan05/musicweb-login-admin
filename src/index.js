b68a474495c69a6e52f584ab3b764d0274f919db
Quoc An <dangquocanf5@gmail.com>
2023-11-30 01:07:36 +0700

    ok sign up , login temp

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { LoginForm } from "./components/LoginForm";
+import { SignUpForm } from "./components/SignUpForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
